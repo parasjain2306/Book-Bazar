33ba7b849608658538cb099713eed34814dd67d8
parasjain2306 <168430323+parasjain2306@users.noreply.github.com>
2024-05-09 19:32:49 +0530

    Update Books.jsx

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -7,7 +7,7 @@ import { useNavigate } from 'react-router-dom'
 
 function Books() {
     const [books,setbooks]=useState()
-    const navigate=useNavigate
+    const navigate=useNavigate()
   useEffect(()=>{
     const fetchData = async () => {
       try {
@@ -20,6 +20,10 @@ function Books() {
   
     fetchData()
   },[])
+    const clickhandler=(index)=>{
+    console.log("indide click");
+     navigate(`/books/${index + 1}`)
+  }
   return (
     <>
     <div className='grid sm:grid-cols-2 gap-4 m-2' >
@@ -39,7 +43,7 @@ function Books() {
 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{object.title}</h5>
 <h4 className='text-xl'>Authors</h4>
 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{object.authors.map((item,ind)=>(<span>{item},</span>))}</p>
-<button onClick={()=>{navigate(`/books/${index + 1}`)}} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+<button onClick={()=>clickhandler(index)} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
 Read more
  <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
